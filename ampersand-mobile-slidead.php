9516afcaceb6c0eb7311efe2dbbe5eeb7605dade
reholmes27 <reholmes27@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-18 20:48:16 +0000

    - update the version to 1.0.4
    git-svn-id: https://plugins.svn.wordpress.org/ampersand-mobile-slidead/tags/1.0.4@1183792 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ampersand-mobile-slidead.php
+++ ampersand-mobile-slidead.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Ampersand Mobile SlideAd
 Description: Add a slideup ad box listing a random selection of chosen posts.
-Version: 1.0.3
+Version: 1.0.4
 Author: Richard Holmes
 Author URI: https://ampersandstudio.uk/
 License: GPL v2 or higher
